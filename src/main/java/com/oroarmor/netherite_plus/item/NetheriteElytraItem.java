50fe0f27760cd2ac1fff95343cd8fdcb3cf28fd2
OroArmor <eliorona@live.com>
2020-07-23 17:47:43 -0700

    Added fixes for trinkets

--- src/main/java/com/oroarmor/netherite_plus/item/NetheriteElytraItem.java
+++ src/main/java/com/oroarmor/netherite_plus/item/NetheriteElytraItem.java
@@ -2,13 +2,23 @@ package com.oroarmor.netherite_plus.item;
 
 import com.oroarmor.util.item.UniqueItemRegistry;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ElytraItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import dev.emi.trinkets.api.Slots;
+import dev.emi.trinkets.api.Trinket;
+import dev.emi.trinkets.api.TrinketComponent;
+import dev.emi.trinkets.api.TrinketsApi;
 
-public class NetheriteElytraItem extends ArmorItem {
+public class NetheriteElytraItem extends ArmorItem implements Trinket {
 
 	public NetheriteElytraItem(Settings settings) {
 		super(NetheriteElytraArmorMaterials.NETHERITE_ELYTRA_MATERIAL, EquipmentSlot.CHEST, settings);
@@ -21,6 +31,34 @@ public class NetheriteElytraItem extends ArmorItem {
 
 	public static boolean isStackUsableAsElytra(ItemStack itemStack) {
 		return UniqueItemRegistry.ELYTRA.isItemInRegistry(itemStack.getItem()) && ElytraItem.isUsable(itemStack);
+	}
+
+	@Override
+	public EquipmentSlot getSlotType() {
+		return EquipmentSlot.CHEST;
+	}
+
+	@Override
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+		if (!FabricLoader.getInstance().isModLoaded("trinkets")) {
+			return super.use(world, player, hand);
+		}
+
+		System.out.println("trinkets");
+
+		ItemStack stack = player.getStackInHand(hand);
+		TrinketComponent comp = TrinketsApi.getTrinketComponent(player);
+		if (comp.equip(stack)) {
+			stack.setCount(0);
+			return new TypedActionResult<ItemStack>(ActionResult.SUCCESS, stack);
+		} else {
+			return new TypedActionResult<ItemStack>(ActionResult.FAIL, stack);
+		}
+	}
+
+	@Override
+	public boolean canWearInSlot(String group, String slot) {
+		return slot.equals(Slots.CAPE);
 	}
 
 }
