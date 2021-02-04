7252334cecae2f8affadddf1a2d5cddf6037e0df
OroArmor <eliorona@live.com>
2021-02-03 19:19:12 -0800

    Fix client crash with rendering and update dependencies

--- common/src/main/java/com/oroarmor/netherite_plus/client/render/NetheritePlusBuiltinItemModelRenderer.java
+++ common/src/main/java/com/oroarmor/netherite_plus/client/render/NetheritePlusBuiltinItemModelRenderer.java
@@ -4,6 +4,7 @@ import com.oroarmor.netherite_plus.block.NetheriteShulkerBoxBlock;
 import com.oroarmor.netherite_plus.client.render.item.NetheriteShieldItemRenderer;
 import com.oroarmor.netherite_plus.client.render.item.NetheriteShulkerBoxItemRenderer;
 import com.oroarmor.netherite_plus.client.render.item.NetheriteTridentItemRenderer;
+import com.oroarmor.netherite_plus.config.NetheritePlusConfig;
 import com.oroarmor.netherite_plus.item.NetheritePlusItems;
 
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -16,9 +17,9 @@ import net.minecraft.item.ItemStack;
 public class NetheritePlusBuiltinItemModelRenderer extends BuiltinModelItemRenderer {
     @Override
     public void render(ItemStack itemStack, ModelTransformation.Mode transformType, MatrixStack poseStack, VertexConsumerProvider multiBufferSource, int i, int j) {
-        if (itemStack.getItem() == NetheritePlusItems.NETHERITE_TRIDENT.get()) {
+        if (NetheritePlusConfig.ENABLED.ENABLED_TRIDENT.getValue() && itemStack.getItem() == NetheritePlusItems.NETHERITE_TRIDENT.get()) {
             NetheriteTridentItemRenderer.render(itemStack, transformType, poseStack, multiBufferSource, i, j);
-        } else if (itemStack.getItem() == NetheritePlusItems.NETHERITE_SHIELD.get()) {
+        } else if (NetheritePlusConfig.ENABLED.ENABLED_SHIELDS.getValue() && itemStack.getItem() == NetheritePlusItems.NETHERITE_SHIELD.get()) {
             NetheriteShieldItemRenderer.render(itemStack, transformType, poseStack, multiBufferSource, i, j);
         } else if (itemStack.getItem() instanceof BlockItem && ((BlockItem) itemStack.getItem()).getBlock() instanceof NetheriteShulkerBoxBlock) {
             NetheriteShulkerBoxItemRenderer.render(itemStack, transformType, poseStack, multiBufferSource, i, j);
