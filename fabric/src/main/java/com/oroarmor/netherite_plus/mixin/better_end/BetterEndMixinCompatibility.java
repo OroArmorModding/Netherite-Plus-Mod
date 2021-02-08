a1f5e252b356c01c62b8f3f68a6542a8c78d3135
OroArmor <eliorona@live.com>
2021-02-07 21:57:30 -0800

    Add better end anvil compatibility

---
+++ fabric/src/main/java/com/oroarmor/netherite_plus/mixin/better_end/BetterEndMixinCompatibility.java
@@ -0,0 +1,51 @@
+package com.oroarmor.netherite_plus.mixin.better_end;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class BetterEndMixinCompatibility implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.equals("com.oroarmor.netherite_plus.mixin.better_end.NetheriteAnvilScreenMixin")) {
+            return FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
+        }
+        return false;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return new ArrayList<String>() {{
+            this.add("better_end.NetheriteAnvilScreenHandlerMixin");
+            this.add("better_end.NetheriteAnvilScreenMixin");
+        }};
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
